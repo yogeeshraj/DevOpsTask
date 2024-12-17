44910f00d860ada60223f9c23cad5abacd39337f
yogeeshraj <95843404+yogeeshraj@users.noreply.github.com>
2024-12-17 18:36:04 +0530

    Update test.js

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,5 +1,25 @@
 const assert = require('assert');
 const http = require('http');
+const { exec } = require('child_process');
+
+let server;
+
+before((done) => {
+    // Start the server before running tests
+    server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World\n');
+    }).listen(3000, '127.0.0.1', () => {
+        console.log('Server running at http://127.0.0.1:3000/');
+        done();
+    });
+});
+
+after((done) => {
+    // Close the server after tests
+    server.close(done);
+});
 
 describe('GET /', () => {
     it('should return Hello World', (done) => {
