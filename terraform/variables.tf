930c39cb89339ee78e997674040b0ed50743c6ff
yogeeshraj <95843404+yogeeshraj@users.noreply.github.com>
2024-12-19 15:52:41 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "default_region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+  default     = "us-west-2"  # You can set a different default value if needed
+}
